eee1f845ef58d220cf88cc0d433daea25ceff438
ZoeyKuan <kuan.zoey.guan.zuyi@gmail.com>
2024-07-10 00:03:11 +0800

    i am back

--- viewGens.js
+++ viewGens.js
@@ -2,26 +2,43 @@
   // LOAD ALL DATA BEFORE THE OTHER JAVASCRIPTS
 
   // getting the items from localstorage
-  var huh = localStorage.getItem('myObject');
-  var myObject = JSON.parse(huh);
-  console.log("local storage sht is "+ myObject[1]);
-  var html = '';
+  const huh = localStorage.getItem('myObject');
+  let myObject = JSON.parse(huh);
+  // console.log("local storage sht is "+ myObject[1]);
+  let html = '';
 
-  dummy = {
+  // reminder to set the null as ''
+  // reminder to set localstorage's object as object holding array of many other objects
+  const dummy = {
     1: {
         "teamACharas": ["Cyno", "Tighnari"],
         "teamBCharas": ["Qiqi", "Kaeya"],
         "boss": "Everlasting Lord of Arcane Wisdom",
         "debuff": ["Can't use e skill", "No Artifacts"],
-        "cryos": [null, null],
-        "diceroll": dice,
-    }
+        "cryos": ['', ''],
+        "diceroll": 21,
+    },
+    2: {
+      "teamACharas": ["Cyno", "Tighnari"],
+      "teamBCharas": ["Qiqi", "Kaeya"],
+      "boss": "Everlasting Lord of Arcane Wisdom",
+      "debuff": ["Can't use e skill", "No Artifacts"],
+      "cryos": ['', ''],
+      "diceroll": 4,
+  }
   };
+
+  document.getElementById('dum').addEventListener('click', loadAllArrays);
 
   // loads all the arrays
   function loadAllArrays() {
-    for (i in dummy.length){
-      // load all the dictionaries
+    // for (var i in dummy){
+    //   // load all the dictionaries
+    //   console.log(i);
+    // }
+    for(i=1;i<3;i++){
+      console.log(dummy[i]);
+      // console.log(dummy[2]);
     }
   }
 
@@ -34,22 +51,11 @@
 
   loadTeamA(dummy[1]["teamAcharas"]);
 
-  function dummyfun() {
-    for (let i = 0; i < dummyData[1]["characters"].length; i++) {
-      console.log(i);
-      console.log(dummyData[1]["characters"][i]);
-    }
-    // for (let i = 0; i < dummyData[1].length; i++) {
-    //   console.log(i);
-    //   console.log(dummyData[1][i]);
-    // };
-  }
-
-  var yes = `Round <span id="round">${a}</span><br>
-    Team A: <span id="teamA">${a}</span><br>
-    Team B: <span id="teamB">${a}</span><br>
-    Boss: <span id="boss">${a}</span><br>
-    Debuff: <span id="debuffA">${a}</span> <span id="debuffB">${a}</span><br><br>`;
+  // var yes = `Round <span id="round">${a}</span><br>
+  //   Team A: <span id="teamA">${a}</span><br>
+  //   Team B: <span id="teamB">${a}</span><br>
+  //   Boss: <span id="boss">${a}</span><br>
+  //   Debuff: <span id="debuffA">${a}</span> <span id="debuffB">${a}</span><br><br>`;
 
   // var teamA = "team A: " + myObject[1]["characters"][0] + ", " + myObject[1]["characters"][1];
   // document.querySelector('#displayingAllRounds').innerHTML = teamA;
