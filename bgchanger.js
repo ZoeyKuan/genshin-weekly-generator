09ca4cbd3d3f2529533f4a2e737f0efbca933ce1
ZoeyKuan <kuan.zoey.guan.zuyi@gmail.com>
2024-09-07 18:49:58 +0800

    added a background image that changes every minute

--- bgchanger.js
+++ bgchanger.js
@@ -1,12 +1,17 @@
 const b = document.querySelector('body');
 let i = 0;
-const changer = ()=>{
- i++;
+let changer = ()=>{
  let bgImages = [
-  'url("icons/bg.png")',
+  'icons/bg.png',
   'https://steamuserimages-a.akamaihd.net/ugc/1673610360171378633/F063BB13044020A4439E53CBC665EA6EB9A72654/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true',
-  'url("icons/archons.png")',
+  'icons/archons.png',
+  'icons/goob.png',
+  'icons/alb.png',
  ];
- b.style.backgroundImage = bgImages[i];
+ b.style.backgroundImage = 'url("' + bgImages[i]+'")';
+ console.log('changed bg', i)
+ if (i == bgImages.length - 1) return i=0;
+ i++;
 };
-setInterval(changer(), 60000);
+var idk = setInterval(changer, 60000);
+//60000
